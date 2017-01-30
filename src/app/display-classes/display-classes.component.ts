f8353eb3c059df4b26c70839e91315ac30e35dab
Amy Hwang and Bill Klinkow <bill@>
2017-01-30 14:07:06 -0800

    add baseline firebase

--- src/app/display-classes/display-classes.component.ts
+++ src/app/display-classes/display-classes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-display-classes',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display-classes.component.css']
 })
 export class DisplayClassesComponent implements OnInit {
-
-  constructor() { }
-
+  students: FirebaseListObservable<any[]>;
+  constructor(private angularFire: AngularFire) {
+    this.students = angularFire.database.list('students');
+  }
   ngOnInit() {
+  }
+
+  getStudents(){
+    return this.students;
   }
 
 }
