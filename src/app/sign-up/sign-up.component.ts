eaac4067b4c295c63fb2b8ad9248c928ce05c7ee
Caitlin Ashtari <caitlinashtari@gmail.com>
2017-01-31 11:26:38 -0800

    add user.name to authentication when creating new user.

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,8 +8,11 @@ import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  users: FirebaseListObservable<any[]>;
 
-  constructor(private af: AngularFire, private router: Router) { }
+  constructor(private af: AngularFire, private router: Router) {
+    this.users = af.database.list('users');
+  }
 
   onSubmit(formData) {
     if(formData.valid) {
@@ -20,7 +23,10 @@ export class SignUpComponent implements OnInit {
       }).then(
         (success) => {
         console.log(success);
-        this.router.navigate(['/login'])
+        this.af.database.list('users').update(success.auth.uid, {
+          name: formData.value.name
+        });
+        this.router.navigate(['/dashboard'])
       }).catch(
         (err) => {
         console.log(err);
